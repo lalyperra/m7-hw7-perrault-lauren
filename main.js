2c9d7e9b154ab790381e18aa98c38938307a671e
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-02-21 12:45:19 -0800

    add code to index, main.css, and main.js
    first run with placeholders and example text

---
+++ main.js
@@ -0,0 +1,33 @@
+{"coord":{"lon":-119.35,
+  "lat":46.3},
+"weather":[
+  {"id":800,
+    "main":"Clear",
+    "description":"clear sky",
+    "icon":"01d"}],
+"base":"stations",
+  "main":{"temp":280.67,
+    "feels_like":277.59,
+    "temp_min":279.82,
+    "temp_max":282.04,
+    "pressure":1025,
+    "humidity":48},
+"visibility":16093,
+"wind":{"speed":1.03,
+    "deg":37},
+"clouds":{"all":1},
+"dt":1582317540,
+"sys":{"type":1,
+    "id":5676,
+    "country":"US",
+    "sunrise":1582296694,
+    "sunset":1582335053},
+  "timezone":-28800,
+    "id":5808276,
+    "name":"Richland",
+    "cod":200}
+
+//print city name at end of header <h1>
+
+
+//print weather description after main section text <p> hint: create text node, append to an element to add to DOM and show on page
