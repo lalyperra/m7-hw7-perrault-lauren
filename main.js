5fff16a37505409e700ca7889c5a5fccab2db9db
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-02-23 16:54:32 -0800

    add API code, css pretty design

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
-{"coord":{"lon":-119.35,
-  "lat":46.3},
-"weather":[
-  {"id":800,
-    "main":"Clear",
-    "description":"clear sky",
-    "icon":"01d"}],
-"base":"stations",
-  "main":{"temp":280.67,
-    "feels_like":277.59,
-    "temp_min":279.82,
-    "temp_max":282.04,
-    "pressure":1025,
-    "humidity":48},
-"visibility":16093,
-"wind":{"speed":1.03,
-    "deg":37},
-"clouds":{"all":1},
-"dt":1582317540,
-"sys":{"type":1,
-    "id":5676,
-    "country":"US",
-    "sunrise":1582296694,
-    "sunset":1582335053},
-  "timezone":-28800,
-    "id":5808276,
-    "name":"Richland",
-    "cod":200}
+var cityName = document.getElementById('place');
 
-//print city name at end of header <h1>
+var weatherDescription = document.getElementById('weather');
 
 
-//print weather description after main section text <p> hint: create text node, append to an element to add to DOM and show on page
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        //Code Dealing With The API Data Goes Here
+
+        //print city name at end of header <h1>
+
+        var locationName= document.createTextNode(apiResult.name);
+        cityName.appendChild(locationName);
+
+        //print weather description after main section text <p> hint: create text node, append to an element to add to DOM and show on page
+        var weatherForecast = document.createTextNode(apiResult.weather[0].description);
+        weatherDescription.appendChild(weatherForecast);
+    }
+};
+
+xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=99353,us&appid=6efff70fe1477748e31c17d1c504635f', true);
+xmlhttp.send();
